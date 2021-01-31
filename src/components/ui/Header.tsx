421a2351632139ffc9f922abbdead97265ab1080
Akila Mendis <akila@oneit.com.au>
2021-01-31 16:18:50 +0530

    018 Styling - Inline

--- src/components/ui/Header.tsx
+++ src/components/ui/Header.tsx
@@ -2,22 +2,34 @@ import React from 'react';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
 import ElevationScroll from './ElevationScroll';
 
 const Header = (props:any) => {
-
+  const classes = useStyles();
   return (
-    <ElevationScroll>
-      <AppBar position="fixed">
-        <Toolbar>
-          <Typography variant="h3">
-            Arc Development
-          </Typography>
-        </Toolbar>
-      </AppBar>
-    </ElevationScroll>
+    <React.Fragment>
+      <ElevationScroll>
+        <AppBar position="fixed">
+          <Toolbar>
+            <Typography variant="h3">
+              Arc Development
+            </Typography>
+          </Toolbar>
+        </AppBar>
+      </ElevationScroll>
+      <div className={classes.toolbarMargin}>
+      </div>
+    </React.Fragment>
   );
 }
 
+const useStyles = makeStyles(theme => (
+  {
+    toolbarMargin: {
+      ...theme.mixins.toolbar
+    }
+  }
+));
 
 export default Header;
