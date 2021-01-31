1c2675a8c3225a60f95db033eb3497113cdcbec1
Akila Mendis <akila@oneit.com.au>
2021-01-31 10:40:10 +0530

    012 The AppBar Component

---
+++ src/components/ui/Header.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+
+const Header = (props:any) => {
+
+  return (
+    <AppBar position="fixed">
+      <Toolbar>
+        Arc Development
+      </Toolbar>
+    </AppBar>
+  );
+}
+
+
+export default Header;
