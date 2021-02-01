a729da90899c08253f7c274ef28ec63c43324998
Akila Mendis <akila@oneit.com.au>
2021-02-01 13:51:26 +0530

    027 Active Tab Refresh Fix

--- src/components/ui/Header.tsx
+++ src/components/ui/Header.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
@@ -18,12 +18,32 @@ const Header = (props:any) => {
     setValue(newValue);
   };
 
+  useEffect(() => {
+    if(window.location.pathname === "/" && value !== 0){
+      setValue(0);
+    }
+    else if(window.location.pathname === "/services" && value !== 1){
+      setValue(1);
+    }
+    else if(window.location.pathname === "/revolution" && value !== 2){
+      setValue(2);
+    }
+    else if(window.location.pathname === "/about" && value !== 3){
+      setValue(3);
+    }
+    else if(window.location.pathname === "/contact" && value !== 4){
+      setValue(4);
+    }
+  },[value]);
+
   return (
     <React.Fragment>
       <ElevationScroll>
         <AppBar position="fixed">
           <Toolbar disableGutters>
-            <img src={logo} alt="company logo" className={classes.logo}/>
+            <Button component={Link} to="/" onClick={()=>{setValue(0)}} className={classes.logoContainer} disableRipple>
+              <img src={logo} alt="company logo" className={classes.logo}/>
+            </Button>
             <Tabs className={classes.tabContainer} value={value} onChange={handleChange} indicatorColor="primary">
               <Tab className={classes.tab} component={Link} to="/" label="Home"/>
               <Tab className={classes.tab} component={Link} to="/services" label="Services"/>
@@ -48,7 +68,7 @@ const useStyles = makeStyles(theme => (
       marginBottom: "3em"
     },
     logo:{
-      height: "7em"
+      height: "8em"
     },
     tabContainer: {
       marginLeft: "auto"
@@ -64,6 +84,12 @@ const useStyles = makeStyles(theme => (
       marginLeft: "50px",
       marginRight: "25px",
       height: "45px",
+    },
+    logoContainer: {
+      padding: 0,
+      "&:hover": {
+        backgroundColor: "transparent"
+      }
     }
   }
 ));
